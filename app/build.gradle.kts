2a3c2d18348031dcd2caddc85179afb9650f1f0a
feivegian <feivegian@gmail.com>
2025-02-03 22:05:25 +0800

    build: Set `compileSdk` and `targetSdk` to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "id.ryenyuku.infinitabs"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "id.ryenyuku.infinitabs"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
