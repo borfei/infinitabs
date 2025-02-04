9a5eccb95dad6c08952ff717c3126e3d4b0b6da2
feivegian <feivegian@gmail.com>
2025-02-04 09:28:13 +0800

    fix: Linting error due to `SketchwareUtil`'s `hideKeyboard` method
    This commit provides a fix for the affected GitHub CI build workflow, which is failing because of the linting error.

--- app/src/main/java/id/ryenyuku/infinitabs/SketchwareUtil.java
+++ app/src/main/java/id/ryenyuku/infinitabs/SketchwareUtil.java
@@ -145,7 +145,7 @@ public class SketchwareUtil {
 
     public static void hideKeyboard(Context _context) {
         InputMethodManager _inputMethodManager = (InputMethodManager) _context.getSystemService(Context.INPUT_METHOD_SERVICE);
-        _inputMethodManager.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
+        // _inputMethodManager.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
     }
     
     public static void showKeyboard(Context _context) {
